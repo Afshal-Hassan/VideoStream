fbff39f284f471eb1f380bb596eff0c88e4f4ef1
AfshalHassan <afshalhassan@venturedive.com>
2023-09-15 15:56:21 +0500

    Video Service is ready testing is pending

--- microservices/VideoService/src/main/java/com/youtube/VideoService/payloads/graphql_domains/outputs/VideoData.java
+++ microservices/VideoService/src/main/java/com/youtube/VideoService/payloads/graphql_domains/outputs/VideoData.java
@@ -1,10 +1,11 @@
 package com.youtube.VideoService.payloads.outputs;
 
+ 
 
 
 
-
-public record VideoData(String title,
+public record VideoData(
+        String title,
         String url,
         Long uploaderId) {
 }
