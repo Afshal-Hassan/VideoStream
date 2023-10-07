86be00e258c94fa26ee43547233d6284627e52bf
AfshalHassan <afshalhassan7@gmail.com>
2023-10-07 23:45:06 +0500

    Integrating api on dashboard

--- YoutubeClone/dashboard/src/main/java/com/youtube/dashboard/config/RepoModule.kt
+++ YoutubeClone/dashboard/src/main/java/com/youtube/dashboard/config/RepoModule.kt
@@ -1,4 +1,21 @@
 package com.youtube.dashboard.config
 
+import com.youtube.dashboard.repos.VideoRepo
+import com.youtube.dashboard.services.VideoService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
 object RepoModule {
+
+
+    @Singleton
+    @Provides
+    fun provideVideoRepo(videoService: VideoService): VideoRepo {
+        return VideoRepo(videoService)
+    }
 }
