c314540310734c8bbe31e0bbfeee0c65b7b18990
AfshalHassan <afshalhassan7@gmail.com>
2023-10-07 23:38:30 +0500

    Integrating api on dashboard

--- YoutubeClone/dashboard/src/main/java/com/youtube/dashboard/viewmodels/VideoViewModel.kt
+++ YoutubeClone/dashboard/src/main/java/com/youtube/dashboard/viewmodels/VideoViewModel.kt
@@ -1,4 +1,28 @@
 package com.youtube.dashboard.viewmodels
 
-class VideoViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.youtube.dashboard.livedata.VideoLiveData
+import com.youtube.dashboard.pojos.VideoResponse
+import com.youtube.dashboard.repos.VideoRepo
+import com.youtube.login.utils.NetworkResult
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class VideoViewModel @Inject constructor(private val videoRepo: VideoRepo) : ViewModel() {
+
+
+    val videoLiveData: LiveData<NetworkResult<VideoResponse>>
+        get() = VideoLiveData.getVideoLiveData()
+
+
+    fun getAllVideos() {
+        viewModelScope.launch {
+            videoRepo.getAllVideos("all")
+        }
+    }
 }
